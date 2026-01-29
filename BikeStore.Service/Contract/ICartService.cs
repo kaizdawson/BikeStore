514154285e862fc5f59cf3f457ca6cd67c09fc02
hoanglmse170482 <hoanglmse170482@fpt.edu.vn>
2026-01-29 23:47:25 +0700

    Api get Cart của user

---
+++ BikeStore.Service/Contract/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using BikeStore.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeStore.Service.Contract
+{
+    public interface ICartService
+    {
+        Task<CartDto> GetMyCartAsync(Guid userId);
+    }
+}
