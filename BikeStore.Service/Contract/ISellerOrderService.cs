6e11d9b0e1169cfa8c22626e9751a8a1a88e4ab8
Kaiz Dawson <kaizdawson@gmail.com>
2026-03-08 17:51:46 +0700

    4 api SellerOrder to change status of orders

---
+++ BikeStore.Service/Contract/ISellerOrderService.cs
@@ -0,0 +1,19 @@
+﻿using BikeStore.Common.DTOs;
+using BikeStore.Common.DTOs.Seller.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeStore.Service.Contract
+{
+    public interface ISellerOrderService
+    {
+        Task<PagedResult<SellerOrderDto>> GetPaidOrdersAsync(Guid sellerId, int pageNumber, int pageSize);
+
+        Task<SellerOrderDto?> ConfirmOrderAsync(Guid sellerId, Guid orderId);
+        Task<SellerOrderDto?> ShipOrderAsync(Guid sellerId, Guid orderId);
+        Task<SellerOrderDto?> CompleteOrderAsync(Guid sellerId, Guid orderId);
+    }
+}
