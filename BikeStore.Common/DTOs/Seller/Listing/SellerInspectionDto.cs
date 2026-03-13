eae36723e5459cb9d8b8976ea39a96b2e0f3b906
Kaiz Dawson <kaizdawson@gmail.com>
2026-03-13 14:48:06 +0700

    get inspector for api listing details

---
+++ BikeStore.Common/DTOs/Seller/Listing/SellerInspectionDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeStore.Common.DTOs.Seller.Listing
+{
+    public class SellerInspectionDto
+    {
+        public bool Frame { get; set; }
+        public bool PaintCondition { get; set; }
+        public bool Drivetrain { get; set; }
+        public bool Brakes { get; set; }
+
+        public int Score { get; set; }
+        public string? Comment { get; set; }
+        public DateTime InspectionDate { get; set; }
+    }
+}
